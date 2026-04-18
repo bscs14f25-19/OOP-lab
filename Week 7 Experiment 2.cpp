ec17e4adca7a01d2210698a3da022cc6f1d8ab87
Aiman Arif <bscs14f25.19@gmail.com>
2026-04-18 11:23:09 +0500

    Week 7 Experiment 2
    Experiment: 2
    Design a class named Month. The class should have the following private members:  name A string object that holds the name of a month, such as “January,” “February,”  monthNumber An integer variable that holds the number of the month. For example, January would
    be 1, February would be 2, etc. Valid values for this variable are 1 through 12. In addition, provide the following member functions: • A default constructor that sets monthNumber to 1 and name to “January.”
    • A constructor that accepts the name of the month as an argument. It should set name to the
    value passed as the argument and set monthNumber to the correct value.
    • A constructor that accepts the number of the month as an argument. It should set monthNumber
    to the value passed as the argument and set name to the correct month name. • Appropriate set and get functions for the name and monthNumber member variables. • Prefix and postfix overloaded ++ operator functions that increment monthNumber and set name to the name of next month. If monthNumber is set to 12 when these functions execute, they should set monthNumber to 1 and name to “January.”
    • Prefix and postfix overloaded − − operator functions that decrement monthNumber and set name to the name of previous month. If monthNumber is set to 1 when these functions execute, they should set monthNumber to 12 and name to “December.”
    • Also, you should overload cout ’s << operator and cin ’s >> operator to work with the Month class. Demonstrate the class in a program.

---
+++ Week 7 Experiment 2.cpp
@@ -0,0 +1,151 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Month {
+private:
+    string name;
+    int monthNumber;
+
+    // Helper function to convert number → name
+    string getMonthName(int num) {
+        string months[] = { "January","February","March","April","May","June",
+                           "July","August","September","October","November","December" };
+        return months[num - 1];
+    }
+
+    // Helper function to convert name → number
+    int getMonthNumber(string n) {
+        string months[] = { "January","February","March","April","May","June",
+                           "July","August","September","October","November","December" };
+        for (int i = 0; i < 12; i++) {
+            if (months[i] == n)
+                return i + 1;
+        }
+        return 1; // default
+    }
+
+public:
+    // Default constructor
+    Month() {
+        monthNumber = 1;
+        name = "January";
+    }
+
+    // Constructor with name
+    Month(string n) {
+        name = n;
+        monthNumber = getMonthNumber(n);
+    }
+
+    // Constructor with number
+    Month(int num) {
+        monthNumber = num;
+        name = getMonthName(num);
+    }
+
+    // Setters
+    void setName(string n) {
+        name = n;
+        monthNumber = getMonthNumber(n);
+    }
+
+    void setMonthNumber(int num) {
+        if (num >= 1 && num <= 12) {
+            monthNumber = num;
+            name = getMonthName(num);
+        }
+    }
+
+    // Getters
+    string getName() {
+        return name;
+    }
+
+    int getMonthNumber() {
+        return monthNumber;
+    }
+
+    // Prefix ++
+    Month operator++() {
+        monthNumber++;
+        if (monthNumber > 12)
+            monthNumber = 1;
+        name = getMonthName(monthNumber);
+        return *this;
+    }
+
+    // Postfix ++
+    Month operator++(int) {
+        Month temp = *this;
+        monthNumber++;
+        if (monthNumber > 12)
+            monthNumber = 1;
+        name = getMonthName(monthNumber);
+        return temp;
+    }
+
+    // Prefix --
+    Month operator--() {
+        monthNumber--;
+        if (monthNumber < 1)
+            monthNumber = 12;
+        name = getMonthName(monthNumber);
+        return *this;
+    }
+
+    // Postfix --
+    Month operator--(int) {
+        Month temp = *this;
+        monthNumber--;
+        if (monthNumber < 1)
+            monthNumber = 12;
+        name = getMonthName(monthNumber);
+        return temp;
+    }
+
+    // Overload <<
+    friend ostream& operator<<(ostream& out, const Month& m) {
+        out << m.name << " (" << m.monthNumber << ")";
+        return out;
+    }
+
+    // Overload >>
+    friend istream& operator>>(istream& in, Month& m) {
+        cout << "Enter month name: ";
+        in >> m.name;
+        m.monthNumber = m.getMonthNumber(m.name);
+        return in;
+    }
+};
+
+int main() {
+    Month m1;            // Default
+    Month m2("March");   // By name
+    Month m3(10);        // By number
+
+    cout << "Default: " << m1 << endl;
+    cout << "By name: " << m2 << endl;
+    cout << "By number: " << m3 << endl;
+
+    // Increment
+    ++m1;
+    cout << "After prefix ++: " << m1 << endl;
+
+    m1++;
+    cout << "After postfix ++: " << m1 << endl;
+
+    // Decrement
+    --m2;
+    cout << "After prefix --: " << m2 << endl;
+
+    m2--;
+    cout << "After postfix --: " << m2 << endl;
+
+    // Input
+    Month m4;
+    cin >> m4;
+    cout << "You entered: " << m4 << endl;
+
+    return 0;
+}
